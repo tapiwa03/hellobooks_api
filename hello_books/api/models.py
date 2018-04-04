64dd29a69c5922288e8a7c6516f877585e5528c1
3V0L <mapfundetl@gmail.com>
2018-04-04 13:40:40 +0300

    clearing white spaces

--- hello_books/api/models.py
+++ hello_books/api/models.py
@@ -43,6 +43,7 @@ class HelloBooks(object):
                 'empty': False,
                 'maxlength': 20,
                 'minlength': 4},
+                
             'email': {
                 'type': 'string',
                 'regex': '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'},
