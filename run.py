895641d18cd866c296c470b6478fca89b8cf94da
Tapiwa Lason Mapfundematsva <lason@Tapiwas-MacBook-Pro.local>
2018-06-21 18:22:14 +0300

    revert to old state to fix error

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
+import os
 from api import create_app
 
 config_name = 'development'
 app = create_app(config_name)
 
 if __name__ == '__main__':
-    app.run()
+    PORT = os.getenv('PORT', 5000)
+    app.run('0.0.0.0',port=PORT)
