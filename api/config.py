0af88b95bc1a4a2e6709cd72714ea7e34cde8203
Tapiwa Lason Mapfundematsva <mapfundetl@gmail.com>
2018-06-22 11:55:13 +0300

    heroku update

--- api/config.py
+++ api/config.py
@@ -18,7 +18,7 @@ class BaseConfig:
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:@localhost/hellobooks'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
